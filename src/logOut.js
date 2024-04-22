a9e16d53fa829b091a322fb3f385e2294c22dd25
valeryzhvalevski <valery.zhvalevski@gmail.com>
2024-04-22 10:53:26 +0300

    login false

--- src/logOut.js
+++ src/logOut.js
@@ -1,17 +1,41 @@
-import { loadFavorites } from "./loadFavorites.js";
-
-document.addEventListener("DOMContentLoaded", () => {
+async function logout() {
+    try {
+      const response = await fetch("http://localhost:3001/users", {
+        method: "GET",
+      });
+  
+      if (!response.ok) {
+        throw new Error("Failed to fetch user data");
+      }
+  
+      const users = await response.json();
+  
+      users.forEach(async (user) => {
+        if (user.login) {
+          user.login = false;
+  
+          const updateUserResponse = await fetch(`http://localhost:3001/users/${user.id}`, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+          });
+  
+          if (!updateUserResponse.ok) {
+            throw new Error("Failed to update user data");
+          }
+        }
+      });
+  
+      window.location.href = "login.html"; 
+    } catch (error) {
+      console.error("Ошибка при выходе:", error);
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
     const logoutButton = document.getElementById("logoutButton");
     logoutButton.addEventListener("click", logout);
-});
-
-function logout() {
-    const favoritesContainer = document.getElementById("favorites-container");
-
-    loadFavorites(favoritesContainer);
-    
-
-    window.location.href = "login.html";
-}
-
-
+  });
+  
